8cd0bf41ff54eae1b54bbe175730e8009d821f0e
melanie rat <RAT.melanie@gmail.com>
2021-06-15 11:55:01 +0200

    add routes 🛣

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  # TODO create routes
+  resources :pins, only: [:index, :show, :new, :create, :update]
+  # TODO create route to choose image
 end
